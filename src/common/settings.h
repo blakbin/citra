c60b0d39c25636ad998772c1692b28dc32c4004f
blakdulz <blakdulz@gmail.com>
2023-05-14 05:28:27 +0700

    avoid android layout order

--- src/common/settings.h
+++ src/common/settings.h
@@ -34,8 +34,8 @@ enum class LayoutOption : u32 {
     SideScreen,
 #ifndef ANDROID
     SeparateWindows,
-#endif
     CustomLayout,
+#endif
     // Similiar to default, but better for mobile devices in portrait mode. Top screen in clamped to
     // the top of the frame, and the bottom screen is enlarged to match the top screen.
     MobilePortrait,
